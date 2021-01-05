ba1e7d0c0ad9327b2c40f107abd23d77718acabd
Mehmet Serkan Ekinci <mehmetserkanekinci@gmail.com>
2021-01-05 09:19:13 +0300

    Dapper Repository added

--- MusicBox.DataAccess/Interfaces/ISPCallRepository.cs
+++ MusicBox.DataAccess/Interfaces/ISPCallRepository.cs
@@ -1,14 +1,13 @@
 ﻿using Dapper;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace MusicBox.DataAccess.Interfaces
 {
     public interface ISPCallRepository : IDisposable
     {
         T Single<T>(string prosedureName, DynamicParameters parameters = null);
-        void Execure(string prosedureName, DynamicParameters parameters = null);
+        void Execute(string prosedureName, DynamicParameters parameters = null);
         T OneRecord<T>(string prosedureName, DynamicParameters parameters = null);
         IEnumerable<T> List<T>(string prosedureName, DynamicParameters parameters = null);
         Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string prosedureName, DynamicParameters parameters = null);
